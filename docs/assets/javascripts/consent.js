49540639ee7c16fb501f222d46b1c14fe7b07e3d
ydkmlt84 <justin@tacohouse.us>
2024-12-28 16:25:31 -0500

    added post consent javascript

--- docs/assets/javascripts/consent.js
+++ docs/assets/javascripts/consent.js
@@ -1,6 +1,6 @@
 var consent = __md_get("__consent")
 if (consent && consent.analytics) {
-  /* The user accepted the cookie */
+    window._paq=window._paq||[];window._paq.push(['rememberCookieConsentGiven']);
 } else {
   /* The user rejected the cookie */
 }
